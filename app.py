dc3bdc9413651930d7cc05e7e3ff5914ff657330
Qosimjon Tojiboev <tbobojon25@gmail.com>
2024-10-06 15:47:32 +0500

    minor update

--- app.py
+++ app.py
@@ -7,13 +7,13 @@ from function import gpt_response, get_weather, user_messages
 app = Client("mybot", api_id=tg_api_id, api_hash=tg_api_hash)
 
 # Список chat_id, которые нужно фильтровать
-allowed_chat_ids = [906893530, 1008114300, 5547028370, 6690844057]  # Замените на свои chat.id
+allowed_user_ids = [906893530, 1008114300, 5547028370, 6690844057]  # Замените на свои chat.id
 
 
 # Создаем пользовательский фильтр
 @filters.create
 def chat_filter(_, __, message):
-    return message.chat.id in allowed_chat_ids
+    return message.from_user.id in allowed_user_ids
 
 
 @app.on_message(filters.command("погода", prefixes=".") & chat_filter)
