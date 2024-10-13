11e3b3d3837fe0d4868bbb612c26df462eef278e
Qosimjon Tojiboev <tbobojon25@gmail.com>
2024-10-13 19:43:20 +0500

    update

--- function.py
+++ function.py
@@ -1,35 +1,8 @@
-import requests
-from g4f.client import Client
-from config import weather_api_key
+from g4f import Provider, ChatCompletion, models
 from collections import defaultdict
 
 
-client = Client()
 user_messages = defaultdict(list)
-
-
-async def get_weather(message, api_key=weather_api_key, city='Худжанд'):
-    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={weather_api_key}&units=metric&lang=ru'
-    response_weather = requests.get(url)
-    if response_weather.status_code == 200:
-        weather_data = response_weather.json()
-        wind_speed = weather_data['wind']['speed']
-        temperature = weather_data['main']['temp']
-        description = weather_data['weather'][0]['description']
-        return await message.reply_text(f'Погода в городе {city}: {description}, Температура: {temperature}°C, '
-                                        f'Скорость ветра:{wind_speed}/с', quote=True)
-    else:
-        return await message.reply_text('Ошибка при получении данных о погоде', response_weather, quote=True)
-
-
-def gpt_response2(message) -> str:
-    user_messages[message.from_user.id].append(message.text)
-    print(user_messages)
-    chat_completion = client.chat.completions.create(model="gpt-4o",
-                                                     messages=[{"role": "user",
-                                                                "content": "Какая ты модель?"}],
-                                                     )
-    return chat_completion.choices[0].message.content or ""
 
 
 def gpt_response(message, text) -> str:
@@ -37,11 +10,12 @@ def gpt_response(message, text) -> str:
     user_messages[user_id].append({"role": "user", "content": text})
     if len(user_messages[user_id]) > 1:
         user_messages[user_id].append({"role": "assistant", "content": user_messages[user_id][-1]["content"]})
-    chat_completion = client.chat.completions.create(
-        model="gpt-4o",
+    gpt_reply = ChatCompletion.create(
+        model=models.gpt_4,
+        provider=Provider.Chatgpt4Online,
         messages=user_messages[user_id]
     )
-    gpt_reply = chat_completion.choices[0].message.content or ''
+
     user_messages[user_id].append({"role": "assistant", "content": gpt_reply})
-    print(user_messages)
+    print(gpt_reply)
     return gpt_reply
