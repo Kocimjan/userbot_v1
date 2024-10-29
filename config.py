9240a4b979922a033b5bc2c5691f6ae1949179cd
Qosimjon Tojiboev <99107806+Kocimjan@users.noreply.github.com>
2024-10-29 10:38:25 +0500

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,8 @@ import os
 
 def get_env_variables():
     # Проверка и получение переменных окружения
-    # tg_api_id = os.getenv('tg_api_id')
-    # tg_api_hash = os.getenv('tg_api_hash')
-
-    tg_api_id = 12585237
-    tg_api_hash = '6518b2191f6d03703cca46a80e7ddef8'
+    tg_api_id = os.getenv('tg_api_id')
+    tg_api_hash = os.getenv('tg_api_hash')
 
     # Если переменные не установлены, запрашиваем их у пользователя
     if not tg_api_id:
